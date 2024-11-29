5139a6ac2b3a4795fdc70220f9590ba2f6ca8b0d
Ahmed S. Lilah <sabrylilah2325667@gmail.com>
2024-11-29 16:14:53 +0200

    2024-11-29 04:15 pm commit

---
+++ 00001-PutsAndGrouping/PutsAndGrouping.tcl
@@ -0,0 +1,19 @@
+# puts: is used to display a value or string to the screen.
+# by default puts outputs to the stdout which is typically the monitor. others my be specified.
+puts Hello
+
+# specifying the output stream to stdout.
+puts stdout Hello
+
+# using quotes for grouping.
+puts "Hello Tcl-Tk"
+
+# using curly brackets for grouping.
+puts {Hello tcl-tk}
+
+#no new line
+puts -nonewline "Hi" 
+puts -nonewline "there" 
+
+
+puts [help puts]
---
+++ 00003-VariablesAndCommands/VariablesAndCommands.tcl
@@ -0,0 +1,24 @@
+# Variable assingnment
+set x 50
+set y 60
+set z x+y
+
+puts z
+
+# Variable substitution '$'
+
+# Command substitution "[]"
+
+
+# identifiers can be alphanumeric and symbols except:
+#       $, @, %, [, ], {, }, ", ;, #, and \
+#       leading special characters - and + 
+
+
+# variables can be and alias to others
+
+
+# set implicitly returns the value of the variable.
+
+# using the set without a second argument only returns the value of variable.
+
